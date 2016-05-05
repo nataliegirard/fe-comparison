4120d649e934a7abe42127f3fee55f4aa1e90442
ngirard <girardnatalie@gmail.com>
2016-05-05 18:14:02 +0000

    add all routes and templates

--- backbone/public/app.js
+++ backbone/public/app.js
@@ -5,9 +5,96 @@ var Contacts = Backbone.Collection.extend({
   url: '/api/contacts'
 });
 
+var ContactsListView = Backbone.View.extend({
+    tagName: 'ul',
+    render: function () {
+        this.collection.forEach(function (model) {
+            this.$el.append( (new ContactsItemView({ model: model })).render().el );
+        }, this);
+        return this;
+    }
+});
+
+var ContactsItemView = Backbone.View.extend({
+    tagName: 'li',
+    events: {
+        'click a': 'handleClick'
+    },
+    template: _.template( $('#ContactsItemViewTemplate').html() ),
+    render: function() {
+        this.el.innerHTML = this.template(this.model.toJSON());
+        return this;
+    },
+    handleClick: function (evt) {
+        evt.preventDefault();
+        Backbone.history.navigate('contacts/' + this.model.get('id'), { trigger: true });
+    }
+});
+
+var ActionsView = Backbone.View.extend({
+   className: 'actions',
+   template: '<a href="/contacts/new">New Contact</a>',
+   render: function () {
+       this.el.innerHTML = this.template;
+       return this;
+   }
+});
+
+var ContactView = Backbone.View.extend({
+    template: _.template( $('#ContactViewTemplate').html() ),
+    initialize: function () {
+        this.listenTo(this.model, 'change:identity change:name', this.updateName);
+    },
+    events: {
+        'click .update': 'update',
+        'click .delete': 'delete'
+    },
+    render: function () {
+        this.el.innerHTML = this.template(this.model.toJSON());
+        return this;
+    },
+    updateName: function () {
+        this.$('h2').text( this.model.get('identity') + ' - ' + this.model.get('name') );
+    },
+    update: function () {
+        this.model.save({
+           identity: this.$('.identity').val(),
+           name: this.$('.name').val(),
+           email: this.$('.email').val()
+        });
+    },
+    delete: function () {
+        this.model.destroy();
+        Backbone.history.navigate('', { trigger: true });
+    }
+});
+
+var NewContactView = Backbone.View.extend({
+    template: $('#NewContactViewTemplate').html(),
+    events: {
+        'click button': 'add'
+    },
+    render: function () {
+        this.el.innerHTML = this.template;
+        return this;
+    },
+    add: function () {
+        this.collection.create({
+           identity: this.$('.identity').val(),
+           name: this.$('.name').val(),
+           email: this.$('.email').val()
+        }, {
+            success: function () {
+                Backbone.history.navigate('', { trigger: true });
+            }
+        });
+    }
+});
+
 var Router = Backbone.Router.extend({
     initialize: function (options) {
         this.contacts = options.contacts;
+        this.el = $('#app');
     },
     routes: {
         '': 'main',
@@ -19,13 +106,17 @@ var Router = Backbone.Router.extend({
         Backbone.history.navigate('contacts', { trigger: true });
     },
     displayList: function () {
-        console.log(this.contacts.pluck('identity'));
+        this.el.empty()
+            .append(new ActionsView().render().el)
+            .append(new ContactsListView({ collection: this.contacts}).render().el);
     },
     displayForm: function () {
-        console.log('form');
+        this.el.empty().append( new NewContactView({ collection: this.contacts }).render().el );
     },
     displaySingle: function (id) {
-        console.log(this.contacts.get(parseInt(id)).toJSON());
+        var model = this.contacts.get( parseInt(id, 10));
+        
+        this.el.empty().append( new ContactView({ model: model }).render().el );
     }
 });
 
